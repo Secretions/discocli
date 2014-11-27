91a15d048867b6493b975bff6f7d74d958c110a9
Joaquin Lopez <mrgus@disco-zombie.net>
2014-11-27 01:38:32 -0800

    Change how we convert new_prompt into instance method

--- python/examples/basic.py
+++ python/examples/basic.py
@@ -2,6 +2,7 @@
 
 import sys
 sys.path.append('../')
+import types # for overriding instance method
 
 import discocli
 
@@ -20,14 +21,7 @@ def normal(cli, tokens):
 cli = discocli.term('Test', debug=False)
 
 # Overriding prompt. Need to convert to instance method to override
-functype = type(cli.prompt)
-# Removed discocli as 3rd argument for python3 compat
-# (not sure what it was for anyway)
-cli.prompt = functype(new_prompt, cli)
-
-# Other way might be like:
-# import types
-# cli.prompt = types.MethodType(new_prompt, cli)
+cli.prompt = types.MethodType(new_prompt, cli)
 
 cli.add_item('print_tokens', 'Print Tokens', print_tokens, context='default')
 cli.add_item('yarnmode', 'Enter "Yarn" Mode', yarnmode, context='default')
