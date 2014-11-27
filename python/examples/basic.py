8e2d43c14b602cb419e7a8a5acefb8523d24aa03
Joaquin Lopez <mrgus@disco-zombie.net>
2014-11-27 01:37:09 -0800

    Remove extra argument for python3 compatibility
    Don't think it should have been there in the first place, the
    only change really being python3 being stricter in the circumstance.

--- python/examples/basic.py
+++ python/examples/basic.py
@@ -9,7 +9,7 @@ def new_prompt(self):
     return '{0} [{1}] -=> '.format(self.name, self.context)
 
 def print_tokens(cli, tokens):
-    print tokens
+    print(tokens)
 
 def yarnmode(cli, tokens):
     cli.context = 'yarn'
@@ -18,8 +18,16 @@ def normal(cli, tokens):
     cli.context = 'default'
 
 cli = discocli.term('Test', debug=False)
+
+# Overriding prompt. Need to convert to instance method to override
 functype = type(cli.prompt)
-cli.prompt = functype(new_prompt, cli, discocli)
+# Removed discocli as 3rd argument for python3 compat
+# (not sure what it was for anyway)
+cli.prompt = functype(new_prompt, cli)
+
+# Other way might be like:
+# import types
+# cli.prompt = types.MethodType(new_prompt, cli)
 
 cli.add_item('print_tokens', 'Print Tokens', print_tokens, context='default')
 cli.add_item('yarnmode', 'Enter "Yarn" Mode', yarnmode, context='default')
